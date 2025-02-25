9b1250c647709c43d94f7278966cf82b029f2f8c
DevShake <dshk0718@gmail.com>
2025-02-25 14:17:19 -0600

    refactor(Scripts): ♻️ Comment out the command lines that were setting the default cursor to the Breeze Dark Red so that users can choose to set the default cursor.

--- post-update.sh
+++ post-update.sh
@@ -3,9 +3,9 @@
 # Allow system file configurations
 sudo -S steamos-readonly disable
 
-# Set the default cursor
-sudo cp -R ./Cursors/Breeze_Dark_Red /usr/share/icons
-sudo sed -i "s/^Inherits=Adwaita*/Inherits=Breeze_Dark_Red/" /usr/share/icons/default/index.theme
+# Set the default cursor; Uncomment if you wish to change your default cursor
+# sudo cp -R ./Cursors/Breeze_Dark_Red /usr/share/icons
+# sudo sed -i "s/^Inherits=Adwaita*/Inherits=Breeze_Dark_Red/" /usr/share/icons/default/index.theme
 
 # Populate Pacman keys
 echo y | sudo pacman-key --init
