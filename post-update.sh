2a2185f701171fb4f50d47e6a71fa8cdf4564a73
DevShake <dshk0718@gmail.com>
2025-02-25 13:30:17 -0600

    feat(Scripts): ✨ Add a command to create a directory `Scripts` if one doesn't already exist.
    Separate some combined commands and add more comments to commands to make the script more clear.

--- post-update.sh
+++ post-update.sh
@@ -7,23 +7,27 @@ sudo -S steamos-readonly disable
 sudo cp -R ./Cursors/Breeze_Dark_Red /usr/share/icons
 sudo sed -i "s/^Inherits=Adwaita*/Inherits=Breeze_Dark_Red/" /usr/share/icons/default/index.theme
 
-# Populate Pacman keys and install necessary tools
+# Populate Pacman keys
 echo y | sudo pacman-key --init
 echo y | sudo pacman-key --populate
-# echo y | sudo pacman -Syyu
-echo y | sudo pacman -S cryfs quota-tools fakeroot
+
+# Install Crypto Filesystem for enabling vault and Disk Quota
+echo y | sudo pacman -S cryfs quota-tools
+# echo y | sudo pacman -Syyu # Enable this if you want to update the system
 
 # Install Yay for easy installation of Arch User apps
+echo y | sudo pacman -S fakeroot
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-sed -i -e 's/pacman>6.1/pacman>6/g' PKGBUILD
+sed -i -e 's/pacman>6.1/pacman>6/g' PKGBUILD # Pacman version fix for installing Yay
 echo y | makepkg -si
 cd ..
 rm -rf ./yay-bin
-sudo ln -s /usr/lib/libalpm.so /usr/lib/libalpm.so.15
+sudo ln -s /usr/lib/libalpm.so /usr/lib/libalpm.so.15 # Link fix for Yay
 echo y | yay -S progress
 
-# Install Tailscale
+# Install Tailscale (See more here https://tailscale.com/)
+mkdir -p /home/deck/Scripts
 cd /home/deck/Scripts
 rm -rf ./deck-tailscale
 git clone https://github.com/tailscale-dev/deck-tailscale.git
@@ -31,7 +35,7 @@ cd ./deck-tailscale
 sudo bash ./tailscale.sh
 source /etc/profile.d/tailscale.sh
 
-# Install Warp Terminal
+# Install Warp Terminal (See more here https://www.warp.dev/)
 sudo rm -R /opt/warpdotdev/warp-terminal
 wget -O /home/deck/Scripts/warp-terminal.pkg.tar.zst https://app.warp.dev/download?package=pacman
 echo y | sudo pacman -U /home/deck/Scripts/warp-terminal.pkg.tar.zst
