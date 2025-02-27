0fc046f981486365e996e9502f86f0f6ab90d894
DevShake <dshk0718@gmail.com>
2025-02-27 04:24:26 -0600

    refactor(Scripts): ♻️ Refactor out the part of the script that was installing `yay` to a separate file so that it can be registered with a separate service later.
    Refactor the `post-update` to run the newly extracted `post-update-user.sh` script for installing `yay` in place of the refactored out part.
    
    Make it possible to run the `post-update` script without `sudo` and add a password prompt using KDE AskPass to securely get the password from the user.

--- post-update.sh
+++ post-update.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Create a folder for storing logs and downloaded repos/files
-mkdir -p /home/deck/.scripts
-
-# Set the log and error file paths
-LOG_FILE=/home/deck/.scripts/post-update.log
-
 # Get the last update's build ID; ignore outputs
 cat /etc/post-update.last-release > /dev/null 2>&1
 if [ $? -eq 0 ]; then
@@ -29,82 +23,104 @@ else
 	echo Current OS BUild "${BUILD_ID}"
 fi
 
+# Prompt for password if only if the current user is `deck`
+# If the current user is root, there's no need for the password prompt
+# This is because the script is most likely running as a service
+# and the service is already running as root
+if [ "$(whoami)" == "deck" ]; then
+	PASSWORD=$(ksshaskpass -- "Please enter your password for sudo operations:" 2> /dev/null)
+	echo "Validating sudo password..."
+	if echo "$PASSWORD" | sudo -S -k -p "" echo "success" &> /dev/null; then
+		echo "Successfully validated sudo password. Continuing..."
+	else
+		echo "Password failed to validate or the user $(whoami) does not have sudo privileges."
+		exit 1
+	fi
+fi
+
+# Create reusable function to run sudo commands with password and yes flag
+run_sudo() {
+	if [ -n "${PASSWORD}" ]; then
+		echo $PASSWORD | sudo -S -k -p "" $@
+	else
+		# Password was not prompted; run the command without password
+		# This is most likely because this script ran from the service as root
+		sudo -n $@ # Make sure to run the command without password prompt
+	fi
+}
+
+# Scripts directory
+SCRIPTS_DIR=/home/deck/.scripts
+
+# Create a folder for storing logs and downloaded repos/files
+mkdir -p ${SCRIPTS_DIR}
+
+# Set the log and error file paths
+LOG_FILE=${SCRIPTS_DIR}/post-update.log
+
 # Create the log and error files
 touch ${LOG_FILE}
 
 # Allow system file configurations
-sudo steamos-readonly disable
+run_sudo steamos-readonly disable
 
 # Set the default cursor; Uncomment if you wish to change your default cursor
-# sudo cp -R ./Cursors/Breeze_Dark_Red /usr/share/icons
-# sudo sed -i "s/^Inherits=Adwaita*/Inherits=Breeze_Dark_Red/" /usr/share/icons/default/index.theme
+# run_sudo cp -R ./Cursors/Breeze_Dark_Red /usr/share/icons
+# run_sudo sed -i "s/^Inherits=Adwaita*/Inherits=Breeze_Dark_Red/" /usr/share/icons/default/index.theme
 
 # Populate Pacman keys
-echo y | sudo pacman-key --init > ${LOG_FILE} 2>&1
+run_sudo pacman-key --init > ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error initializing Pacman keys."
 	exit 1
 fi
-echo y | sudo pacman-key --populate >> ${LOG_FILE} 2>&1
+run_sudo pacman-key --populate >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error populating Pacman keys."
 	exit 1
 fi
 
 # Install Crypto Filesystem for enabling vault and Disk Quota
-echo y | sudo pacman -S cryfs quota-tools >> ${LOG_FILE} 2>&1
+run_sudo pacman --noconfirm --noprogressbar -S cryfs quota-tools >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error installing cryfs and quota-tools."
 	exit 1
 fi
 
 # Uncomment below to do full system upgrade
-# echo y | sudo pacman -Syyu >> ${LOG_FILE} 2>&1
+# run_sudo pacman --noconfirm --noprogressbar -Syyu >> ${LOG_FILE} 2>&1
 # if [ $? -ne 0 ]; then
 # 	echo "Error updating the system."
 # 	exit 1
 # fi
 
 # Install Yay for easy installation of Arch User apps
-echo y | sudo pacman -S fakeroot >> ${LOG_FILE} 2>&1
+# Install fakeroot dependency for Yay
+run_sudo pacman --noconfirm --noprogressbar -S fakeroot >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error installing fakeroot."
 	exit 1
 fi
-# Change to `/home/deck/.scripts` directory before installing any user apps
-cd /home/deck/.scripts
-rm -rf ./yay-bin
-git clone https://aur.archlinux.org/yay-bin.git >> ${LOG_FILE} 2>&1
-if [ $? -ne 0 ]; then
-	echo "Error cloning yay-bin repository."
-	exit 1
-fi
-cd yay-bin
-# Pacman version fix for installing Yay
-sed -i -e 's/pacman>6.1/pacman>6/g' PKGBUILD >> ${LOG_FILE} 2>&1
-if [ $? -ne 0 ]; then
-	echo "Error fixing the PKGBUILD file for Yay."
-	exit 1
-fi
-echo y | makepkg -si >> ${LOG_FILE} 2>&1
-if [ $? -ne 0 ]; then
-	echo "Error installing Yay."
-	exit 1
-fi
-cd ..
 # Link fix for Yay
-sudo ln -sf /usr/lib/libalpm.so /usr/lib/libalpm.so.15 >> ${LOG_FILE} 2>&1
+run_sudo ln -sf /usr/lib/libalpm.so /usr/lib/libalpm.so.15 >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error linking libalpm.so to libalpm.so.15."
 	exit 1
 fi
-echo y | yay -S progress >> ${LOG_FILE} 2>&1
-if [ $? -ne 0 ]; then
-	echo "Error installing progress via yay."
-	exit 1
+# Run the script for installing Yay; only run it if the current user is `deck`.
+# This is due to the  `makepkg` command having a hard limitation against the root user
+# running the command to prevent any potential permanent damage to the system by design.
+CURRENT_USER=$(whoami)
+if [ "${CURRENT_USER}" == "deck" ]; then
+	echo $PASSWORD | ./post-update-user.sh >> ${LOG_FILE} 2>&1
+	if [ $? -ne 0 ]; then
+		echo "Error running post-update-user.sh for installing Yay."
+		exit 1
+	fi
 fi
-rm -rf ./yay-bin
 
+# Change to `SCRIPTS_DIR` directory before installing any user apps
+cd ${SCRIPTS_DIR}
 # Install Tailscale (See more here https://tailscale.com/)
 rm -rf ./deck-tailscale
 git clone https://github.com/tailscale-dev/deck-tailscale.git >> ${LOG_FILE} 2>&1
@@ -113,7 +129,12 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd deck-tailscale
-sudo bash ./tailscale.sh >> ${LOG_FILE} 2>&1
+sed -i "s/^wget -q --show-progress*/wget -q/" ./tailscale.sh >> ${LOG_FILE} 2>&1
+if [ $? -ne 0 ]; then
+	echo "Error modifying the tailscale.sh script."
+	exit 1
+fi
+run_sudo bash ./tailscale.sh >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error installing Tailscale."
 	exit 1
@@ -126,23 +147,25 @@ fi
 rm -rf ./deck-tailscale
 
 # Install Warp Terminal (See more here https://www.warp.dev/)
-sudo rm -rf /opt/warpdotdev/warp-terminal
-wget -O /home/deck/.scripts/warp-terminal.pkg.tar.zst https://app.warp.dev/download?package=pacman >> ${LOG_FILE} 2>&1
+run_sudo rm -rf /opt/warpdotdev/warp-terminal
+wget -q -O ${SCRIPTS_DIR}/warp-terminal.pkg.tar.zst https://app.warp.dev/download?package=pacman >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error downloading Warp Terminal package."
 	exit 1
 fi
-echo y | sudo pacman -U /home/deck/.scripts/warp-terminal.pkg.tar.zst >> ${LOG_FILE} 2>&1
+run_sudo pacman --noconfirm --noprogressbar -U ${SCRIPTS_DIR}/warp-terminal.pkg.tar.zst >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error installing Warp Terminal."
 	exit 1
 fi
-rm /home/deck/.scripts/warp-terminal.pkg.tar.zst
+rm ${SCRIPTS_DIR}/warp-terminal.pkg.tar.zst
 
 # Store the current OS update version to compare to later; ignore output
-sudo tee /etc/post-update.last-release < /etc/os-release 1> /dev/null
+sudo -S tee /etc/post-update.last-release < /etc/os-release 1> /dev/null
 
 # Set the system back to read-only
-sudo steamos-readonly enable
+run_sudo steamos-readonly enable
+
+echo "Post-update completed successfully."
 
 exit 0
